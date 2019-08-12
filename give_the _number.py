e9a3040c7be0ac6fdc523d5b390655d34acb7cb2
diving16 <daiweiying@seu.edu.cn>
2019-08-12 17:47:25 +0800

    Update give_the _number.py
    等价鞅：https://zh.wikipedia.org/wiki/%E9%9E%85_(%E6%A6%82%E7%8E%87%E8%AE%BA)

--- give_the _number.py
+++ give_the _number.py
@@ -19,36 +19,97 @@ def give_the_number():
 #     print(result)
 # =============================================================================
 
-#定义一个赌徒
-def a_fool(funds,wager,game_count):
-    current_game_count=0
+def a_smart_fool(funds,initial_wager,game_count):
+    wager=initial_wager
     x=[]
     y=[]
+    current_game_count=0
+    previous_game='win'
+    previous_game_wager=initial_wager
     
-    while current_game_count<game_count:
-        if give_the_number():
-            funds+=wager
-            x.append(current_game_count)
-            y.append(funds)
-        else:
-            funds-=wager
-            x.append(current_game_count)
-            y.append(funds)
+    while current_game_count<=game_count:
+        if previous_game == 'win':
+            if give_the_number():
+                previous_game='win'
+                funds+=wager
+                print('earn {}'.format(wager))
+                x.append(current_game_count)
+                y.append(funds)
+            else:
+                previous_game='lose'
+                funds-=wager
+                print('lose {}'.format(wager))
+                x.append(current_game_count)
+                y.append(funds)
 
+                if funds<0:
+                    print('bye,you have played {} games'.format(current_game_count))
+                    break              
+        elif previous_game == 'lose':
+            if give_the_number():
+                wager=previous_game_wager*2
+                previous_game='win'
+                wager=initial_wager
+                funds+=wager
+                print('earn {}'.format(wager))               
+                x.append(current_game_count)
+                y.append(funds)
+                         
+            else:
+                wager=previous_game_wager*2
+                previous_game='lose'
+                funds-=wager
+                print('lose {}'.format(wager))               
+                x.append(current_game_count)
+                y.append(funds)                
+                if funds<0:
+                    print('bye,you have played {} games'.format(current_game_count))
+                    break
+       
         current_game_count+=1
-        
-    if funds<0:
-        funds='Penny less'
+    print(funds)
     plt.plot(x,y)
-    plt.show()
-#    print('how rich:', funds)
 
-#从下面的蒙特卡洛仿真来看，玩的次数越多，大家都会亏损。 
 n=0
-while n<=100:
-    a_fool(1000,50,100000)
+while n<100:
+    a_smart_fool_1=a_smart_fool(10000,1000,100)
     n+=1
 plt.xlabel('how many games take part in')
 plt.xlabel('Funds')
 plt.show()
+
+# =============================================================================
+# #定义一个赌徒
+# def a_fool(funds,wager,game_count):
+#     current_game_count=0
+#     x=[]
+#     y=[]
+#     
+#     while current_game_count<game_count:
+#         if give_the_number():
+#             funds+=wager
+#             x.append(current_game_count)
+#             y.append(funds)
+#         else:
+#             funds-=wager
+#             x.append(current_game_count)
+#             y.append(funds)
+# 
+#         current_game_count+=1
+#         
+#     if funds<0:
+#         funds='Pennyless'
+#     plt.plot(x,y)
+#     plt.show()
+# #    print('how rich:', funds)
+# 
+# #从下面的蒙特卡洛仿真来看，玩的次数越多，大家都会亏损。 
+# n=0
+# while n<=100:
+#     a_fool(1000,50,100000)
+#     n+=1
+# plt.xlabel('how many games take part in')
+# plt.xlabel('Funds')
+# plt.show()
+# =============================================================================
     
